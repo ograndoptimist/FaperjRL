10ef358114501a2c9e27cffb5a900e750a343b34
Gabriel Pontes <33611916+ograndoptimist@users.noreply.github.com>
2019-08-30 23:56:48 -0300

    now uses vectorizer function

--- source/train_agent.py
+++ source/train_agent.py
@@ -4,6 +4,12 @@ from keras.preprocessing.sequence import pad_sequences
 
 from source.emulador.emulador import AdmiravelMundoNovo
 
+from source.agent.memory_buffer import MemoryReplay
+
+from source.agent.dqn_agent import DQNAgent
+
+from source.preprocessing.preprocessing import vectorizer 
+
 
 def treino(episodios=256,
            batch_size=64,
@@ -25,29 +31,23 @@ def treino(episodios=256,
     """
     eps = epsilon
 
-    # Dicionário que armazena {episódio:reforco_acumulado}
-    estat_reforcos = dict()
+    memory_replay = MemoryReplay(buffer_size=50)
 
-    # Tentativa de implementação do replay de experiência.
-    memoria_replay = dict()
-    vet_estado_acao_target = [[None], [None], [None]]
+    dqn_agent = DQNAgent()
 
     numero_iteracoes = 0
-
 
     for episodio in range(1, episodios + 1):
         reforco_acumulado = 0
 
-        # O jogo é inicializado no primeiro estado.
         jogo = AdmiravelMundoNovo()
 
         for passo in range(batch_size):
             # Obtém as descrições de estado e ação do jogo.
-            estado_texto, acao_texto, dimensao_acao = jogo.read()
+            state_text, action_text, dimensao_acao = jogo.read()
 
-            # Pré-processa os estados e ações, que estão em texto, para vetores de inteiros.
-            estado_ = transforma(estado_texto)
-            acoes_ = transforma(acao_texto)
+            estado_ = vectorizer(state_text)
+            acoes_ = vectorizer(action_text)
 
             # Seleciona uma ação, escolhida de forma aleatória ou através de max(Q(s, a)).
             if np.random.random() < eps:
@@ -56,12 +56,10 @@ def treino(episodios=256,
                 escolha = np.argmax(self.Q(estado_, acoes_))
 
             # Executa a ação no emulador e observa o reforço e o próximo estado.
-            proximo_estado_texto, proximas_acoes_texto, prox_dimensao_acao, prox_reforco, terminado = jogo.emulador(
-                escolha)
+            proximo_estado_texto, proximas_acoes_texto, prox_dimensao_acao, prox_reforco, terminado = jogo.emulador(escolha)
 
-            # Pré-processa os próximos estados e ações, que estão em texto, para vetores de inteiros.
-            proximo_estado = transforma(proximo_estado_texto)
-            proximas_acoes = transforma(proximas_acoes_texto)
+            proximo_estado = vectorizer(proximo_estado_texto)
+            proximas_acoes = vectorizer(proximas_acoes_texto)
 
             if not terminado:
                 # Q(s, a) = r' +  γ  * Q(s', a')
@@ -69,50 +67,22 @@ def treino(episodios=256,
             else:
                 target = prox_reforco
 
-            # Para o treinamento ser realizado em batch, é necessário que:
-            vet_estado_acao_target[0] = estado_
-            vet_estado_acao_target[1] = acoes_ if isinstance(acoes_[0], int) else acoes_[escolha]
-            vet_estado_acao_target[2] = [target]
-
-            memoria_replay[numero_iteracoes] = vet_estado_acao_target.copy()
-
+            memory_replay.add_item(state=estado,
+                                   action=acoes_[escolha],
+                                   reinforcement=target)
+            
             reforco_acumulado += prox_reforco
 
             if terminado:
                 break
 
-            # Dessa vez, a transição de estado é realizada:
-            # estado = novo_estado
             jogo.transicao_estado(escolha)
 
             numero_iteracoes += 1
 
-        # Torna os dados estatisticamente independentes, já que os dados num problema de RL
-        # são fortemente dependentes e não-uniformemente distribuídos.
-        amostra = list(memoria_replay.values())
-        if episodio in [1, 2, 3, 4, 5, 6]:
-            random.shuffle(amostra)
-        elif episodio < 50:
-            amostra = random.sample(amostra, batch_size // 8)
-        else:
-            main = random.sample(amostra, batch_size // 4)
+        state_array, action_array, reinforcement_array = memory_replay.to_numpy()
 
-        estado = []
-        acao = []
-        Q_target = []
-        for i in range(len(amostra)):
-            estado.append(amostra[i][0])
-            acao.append(amostra[i][1])
-            Q_target.append(amostra[i][2])
-
-        # As sequências precisam ter o mesmo tamanho dentro do mesmo tensor que vai alimentar a rede neural.
-        estado = pad_sequences(estado)
-        acao = pad_sequences(acao)
-
-        Q_target = np.array(Q_target)
-
-        # Realiza o passo de gradiente de descida de forma a alcançar o mínimo global da função.
-        self.modelo.fit([estado, acao], Q_target, epochs=5, verbose=False)
+        dqn_agent.fit([state_array, action_array], reinforcement_array, epochs=5, verbose=False)
 
         print("Episódio {0}: Reforço acumulado de {1}".format(episodio, reforco_acumulado))
         print()
